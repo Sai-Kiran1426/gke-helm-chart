0b44abc83dc023bad94f27418120a3bc03dd4cf0
Sai-Kiran1426 <saikiran.m9498@gmail.com>
2025-09-22 16:19:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python on GKE with Helm"
+    return "Hello from Python on GKE with Helm Chart"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
